aa590f3d5776deb387f60f5bf6ded044e7146f97
ryuk200tr <61316400+ryuk200tr@users.noreply.github.com>
2020-09-23 00:26:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 app.use(express.static('public'));
-app.listen(8000)
+let port = process.env.PORT || 3000;
 app.get('/', (request, response) => {
 
  response.json({ip: '217.131.114.104'})
