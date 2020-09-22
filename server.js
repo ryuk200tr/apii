e9c9a7e841763c27e2f5c9dc8613e3ebf39d9165
ryuk200tr <61316400+ryuk200tr@users.noreply.github.com>
2020-09-23 00:40:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-app.use(express.static('public'));
-let port = process.env.PORT || 3000;
-app.get('/', (request, response) => {
+  
+const { exec } = require('child_process');
 
- response.json({ip: '217.131.114.104'})
-})
+// Set port (default: 3000). For Heroku, we need to use
+// the port set by the environment variable $PORT
+const port = process.env.PORT || 3000;
+
+const command = `json-server --watch db.json --port ${port}`;
+
+exec(command, (err, stdout, stderr) => {
+  if (err) {
+    console.log('Error running exec', err);
+    return;
+  }
+  console.log('stdout:', stdout);
+  console.log('stderr:', stderr);
+});
